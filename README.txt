3ce1650b32483fec254e03f9841134377126cabf
Craig Meinschein <cmeinsch@gmail.com>
2012-08-02 10:39:00 -0700

    Putting new SCCM update script on Github.

---
+++ README.txt
@@ -0,0 +1,12 @@
+SCCM 2007 Machine Policy Update Script
+By: Craig Meinschein
+
+Forces a computer (or set of computers) to refresh their
+SCCM Machine Policy. In my environment, it was easiest to implement this
+as a PowerShell script with a batch script "wrapper".
+
+Usage:
+
+sccmupdate.cmd [computer1] [computer2] [...]
+
+If run with no arguments, it will update Machine Policy on the local machine.
---
+++ sccmupdate.cmd
@@ -0,0 +1,21 @@
+@echo off
+REM Refresh SCCM Machine Policy on a computer or set of computers.
+REM If target computer is not specified, SCCM Machine Policy is refreshed
+REM on the local machine.
+REM
+REM Usage: sccmupdate.cmd [computer1] [computer2] [computer3] [...]
+
+if "%1"=="" (
+	powershell.exe -Command "\\cecs\resources\programs\programs\tools\SCCMupdate.ps1 %COMPUTERNAME%"
+)
+:loop
+if "%1"=="" goto continue
+ping /n 1 /w 300 %1 > nul
+if errorlevel 1 (
+	echo %1 is down.
+) else (
+	powershell.exe -Command "\\cecs\resources\programs\programs\tools\SCCMupdate.ps1 %1"
+)
+shift
+goto loop
+:continue
